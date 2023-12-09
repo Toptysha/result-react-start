5abfecc0e502827e266db8e0c2eb019b6ba322ec
Vladislav Baranov <toptun113@gmail.com>
2023-12-09 20:56:45 +0300

    refactor app.js

--- init-react-app/src/App.js
+++ init-react-app/src/App.js
@@ -1,57 +1,75 @@
+import {createElement} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const app = document.createElement('div')
-  app.className = 'App'
-  document.body.append(app)
+  // const app = document.createElement('div')
+  // app.className = 'App'
+  // document.body.append(app)
 
-  const header = document.createElement('header')
-  header.className = 'App-header'
-  app.append(header)
+  // const header = document.createElement('header')
+  // header.className = 'App-header'
+  // app.append(header)
 
-  const imgLogo = document.createElement('img')
-  imgLogo.src = logo
-  imgLogo.className = 'App-logo'
-  imgLogo.alt = 'logo'
-  header.append(imgLogo)
+  // const imgLogo = document.createElement('img')
+  // imgLogo.src = logo
+  // imgLogo.className = 'App-logo'
+  // imgLogo.alt = 'logo'
+  // header.append(imgLogo)
 
-  const p = document.createElement('p')
-  p.textContent = 'Edit src/App.js and save to reload.'
-  header.append(p)
+  // const p = document.createElement('p')
+  // p.textContent = 'Edit src/App.js and save to reload.'
+  // header.append(p)
 
-  const link = document.createElement('a')
-  link.className = 'App-link'
-  link.href = "https://reactjs.org"
-  link.target="_blank"
-  link.rel="noopener noreferrer"
-  link.textContent = 'Learn React'
-  header.append(link)
+  // const link = document.createElement('a')
+  // link.className = 'App-link'
+  // link.href = "https://reactjs.org"
+  // link.target="_blank"
+  // link.rel="noopener noreferrer"
+  // link.textContent = 'Learn React'
+  // header.append(link)
 
-  const year = document.createElement('h2')
-  year.textContent = new Date().getFullYear()
-  header.append(year)
+  // const year = document.createElement('h2')
+  // year.textContent = new Date().getFullYear()
+  // header.append(year)
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //       <h2>{new Date().getFullYear()}</h2>
-  //     </header>
-  //   </div>
-  // );
+  return (
+    createElement('div', {className: "App"}, 
+      createElement('header', {className: "App-header"}, 
+        createElement('img', {src: logo, className: "App-logo", alt: "logo"}),
+        createElement('p', null, 
+          'Edit',
+          createElement('code', null, 'src/App.js'), 
+          'and save to reload.'
+        ),
+        createElement('a', {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer"
+        }, 'Learn React'),
+        createElement('h2', null, new Date().getFullYear())
+      )
+    )
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <h2>{new Date().getFullYear()}</h2>
+    //   </header>
+    // </div>
+  );
 }
 
 export default App;
